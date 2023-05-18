64d420f4d7a1c2e6a4dbea53a8005495251612c4
Raiki Kudo <105319451+KudouRaiki1016@users.noreply.github.com>
2023-05-18 14:25:31 +0900

    test_push

--- jinrou/jinrouapp/urls.py
+++ jinrou/jinrouapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 urlpatterns = [
     path('start/', ), # スタート画面
+    path('stay/',), # 待機画面
     path('setting/', ), # 設定画面
     path('role/', ), # 役職決定画面
     path('action', ), # 夜の行動画面
