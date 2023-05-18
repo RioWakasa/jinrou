7e6c8706c1bde1dfd12170d8b31b7c39c886113b
RioWakasa <103770969+RioWakasa@users.noreply.github.com>
2023-05-18 13:46:14 +0900

    first commit

---
+++ jinrou/jinrouapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+urlpatterns = [
+    path('start/', ), # スタート画面
+    path('setting/', ), # 設定画面
+    path('role/', ), # 役職決定画面
+    path('action', ), # 夜の行動画面
+    path('discussion/', ), # 話し合い画面
+    path('select/', ), # 吊り選択画面
+    path('result/', ), # 結果画面
+]
